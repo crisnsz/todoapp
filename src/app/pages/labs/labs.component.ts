2a97ddd2b75b48f41c5192647b93ed8de6c5af11
Cristian Sanchez <cristian.sanchez.fs@outlook.com>
2023-11-28 09:18:34 -0600

    Merge local to cloud proyect.

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -11,23 +11,64 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './labs.component.css',
 })
 export class LabsComponent {
-  name = signal('Ana');
-  age = 24;
+  welcome = 'hola';
   tasks = signal([
-    {
-      id: Date.now(),
-      title: 'Instalar Angular',
-      completed: false,
-    },
-    {
-      id: Date.now(),
-      title: 'Crear Componentes',
-      completed: false,
-    },
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componentes',
   ]);
 
+  name = signal('Cristian');
+  age = 24;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  person = signal({
+    name: 'Cristian',
+    age: 24,
+    disabled: true,
+    img: 'https://w3schools.com/howto/img_avatar.png',
+  });
+
+  clickHandler() {
+    alert('Hola');
+  }
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+
+    console.log(event);
+  }
+
+  changeAge(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+
+    this.person.update((prevState) => {
+      return { ...prevState, age: parseInt(newValue) };
+    });
+
+    console.log(event);
+  }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+
+  changeName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+
+    this.person.update((prevState) => {
+      return { ...prevState, name: newValue };
+    });
+  }
+
   colorCtrl = new FormControl();
-  widthCtrl = new FormControl('', {
+  widthCtrl = new FormControl('20', {
     nonNullable: true,
   });
 
